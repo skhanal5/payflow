d4a8c8185e5c3eb9ca9905502682208782b58091
Subodh Khanal <skhanal5@terpmail.umd.edu>
2025-05-11 15:20:03 -0400

    Add logic to store orders into db using gorm

---
+++ internal/order/repository/model.go
@@ -0,0 +1,22 @@
+package repository
+
+import "gorm.io/gorm"
+
+type Order struct {
+	gorm.Model
+	UserId   string `gorm:"unique;not null"`
+	Status string `gorm:"not null"`
+	OrderItems []OrderItem
+}
+
+type OrderItem struct {
+	gorm.Model
+	OrderId string `gorm:"not null"`
+	ProductId string `gorm:"not null"`	
+	Quantity int `gorm:"not null"`
+	Price float64 `gorm:"not null"`
+}
+
+type OrderStatus struct {
+	ID string
+}
---
+++ internal/order/repository/order.go
@@ -0,0 +1,47 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+    "gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type OrderDB struct {
+	db *gorm.DB
+}
+
+type OrderRepository interface {
+	InsertOrder(ctx context.Context, order *Order) (*Order, error)
+	GetOrder(ctx context.Context, orderID string) (*OrderStatus, error)
+}
+
+func DefineGormDSN(host string, user string, password string, port string) string {
+	return fmt.Sprintf("host=%s user=%s password=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", host, user, password, port)	
+}
+
+func NewOrderDB(host string, user string, password string, port string) *OrderDB {
+	dsn := DefineGormDSN(host, user, password, port)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	
+	if err != nil {
+		//TODO: Add error message
+		panic("failed to connect to Order database")
+	}	
+	return &OrderDB{db: db}
+}
+
+func (db *OrderDB) InsertOrder(ctx context.Context, order *Order) (*Order, error) {
+	if err := db.db.WithContext(ctx).Create(order).Error; err != nil {
+		return nil, err
+	}
+	return order, nil
+}
+
+func (db *OrderDB) GetOrder(ctx context.Context, orderID string) (*OrderStatus, error) {
+	var order OrderStatus
+	if err := db.db.WithContext(ctx).Where("order_id = ?", orderID).First(&order).Error; err != nil {
+		return nil, err
+	}
+	return &order, nil
+}
