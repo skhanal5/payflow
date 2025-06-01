2936216f136fc17e6e5bb50db798eb054f02199f
Subodh Khanal <skhanal5@terpmail.umd.edu>
2025-06-01 13:13:05 -0400

    Add shared helper functions in utility package for now

---
+++ internal/utility/env.go
@@ -0,0 +1,13 @@
+package utility
+
+import (
+	"fmt"
+	"os"
+)
+
+func GetEnvOrPanic(key string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	panic(fmt.Sprintf("required environment variable %q not set", key)) //TODO: Use logger
+}
