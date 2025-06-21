c26c76cc1b7c94fab4737117c02a0914e24f267c
Subodh Khanal <skhanal5@terpmail.umd.edu>
2025-06-20 21:09:18 -0400

    Rename entrypoint packages to match internal package names

--- cmd/payflow/product/main.go
+++ cmd/payflow/product/main.go
@@ -4,15 +4,15 @@ import (
 	"context"
 
 	kafkaclient "github.com/segmentio/kafka-go"
-	"github.com/skhanal5/payflow/internal/inventory/config"
-	"github.com/skhanal5/payflow/internal/inventory/kafka"
-	"github.com/skhanal5/payflow/internal/inventory/repository"
+	"github.com/skhanal5/payflow/internal/product/config"
+	"github.com/skhanal5/payflow/internal/product/kafka"
+	"github.com/skhanal5/payflow/internal/product/repository"
 	"github.com/skhanal5/payflow/internal/shared"
 )
 
 func main() {
 	cfg := config.NewConfig()
-	db := repository.NewInventoryDB(cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBPassword)
+	db := repository.NewProductDB(cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBPassword)
 	reader := kafkaclient.NewReader(kafkaclient.ReaderConfig{
 		Brokers: []string{cfg.KafkaBroker},
 		GroupID: cfg.KafkaGroupId,
@@ -22,7 +22,7 @@ func main() {
 		Addr:     kafkaclient.TCP(cfg.KafkaBroker),
 		Balancer: &kafkaclient.LeastBytes{},
 	}
-	logger := shared.InitLogger("inventory-service", cfg.Environment)
+	logger := shared.InitLogger("product-service", cfg.Environment)
 	processor := kafka.NewInventoryProcessor(
 		cfg.InventoryCheckedTopic,
 		reader,
