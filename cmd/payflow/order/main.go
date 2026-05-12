bd9eb8a89d9fd19224e4b05bf39d0b730aaf9ce5
Subodh Khanal <skhanal5@proton.me>
2026-05-12 16:58:34 -0700

    Wire up Kafka processor in product service and update entrypoints

--- cmd/payflow/order/main.go
+++ cmd/payflow/order/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
+
 	"github.com/skhanal5/payflow/internal/order/config"
+	"github.com/skhanal5/payflow/internal/order/kafka"
 	"github.com/skhanal5/payflow/internal/order/repository"
 	"github.com/skhanal5/payflow/internal/order/server"
 	"github.com/skhanal5/payflow/internal/order/service"
@@ -11,8 +14,19 @@ import (
 func main() {
 	cfg := config.NewConfig()
 	log := logger.InitLogger("order-service", cfg.Environment)
+
 	db := repository.NewOrderDB(cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBPort)
-	orderService := service.NewOrderService(db, &log)
+
+	producer := kafka.NewOrderWriter([]string{cfg.KafkaBroker}, cfg.OrderRequestedTopic, &log)
+	orderService := service.NewOrderService(db, producer, &log)
+
+	consumer := kafka.NewOrderReader([]string{cfg.KafkaBroker}, cfg.KafkaGroupId, []string{cfg.InventoryCheckedTopic}, db, &log)
+	go func() {
+		log.Info().Msg("Starting inventory results consumer")
+		if err := consumer.ProcessInventoryResults(context.Background()); err != nil {
+			log.Fatal().Err(err).Msg("Inventory results consumer failed")
+		}
+	}()
 
 	server.StartServer(cfg.GRPCPort, log, orderService)
 }
