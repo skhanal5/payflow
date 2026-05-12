8d04b93ac818d92ea05d18f5fd5dbf238146c1b6
Subodh Khanal <skhanal5@proton.me>
2026-05-12 16:43:31 -0700

    Update order models: add user_id, shipping_address, price fields

--- internal/order/repository/model.go
+++ internal/order/repository/model.go
@@ -4,15 +4,17 @@ import "gorm.io/gorm"
 
 type Order struct {
 	gorm.Model
-	OrderId    string      `gorm:"unique;not null"`
-	Status     string      `gorm:"not null"`
-	OrderItems []OrderItem `gorm:"foreignKey:OrderId"`
+	OrderId         string      `gorm:"unique;not null"`
+	UserId          string      `gorm:"not null"`
+	Status          string      `gorm:"not null"`
+	ShippingAddress string      `gorm:"not null"`
+	OrderItems      []OrderItem `gorm:"foreignKey:OrderId"`
 }
 
 type OrderItem struct {
 	gorm.Model
-	OrderId   string `gorm:"not null"`
-	ProductId string `gorm:"not null"`
-	Quantity  int    `gorm:"not null"`
-	// Price float64 `gorm:"not null"`
+	OrderId   string  `gorm:"not null"`
+	ProductId string  `gorm:"not null"`
+	Quantity  int     `gorm:"not null"`
+	Price     float64 `gorm:"not null"`
 }
